615b22e67ee6ba212970e2ae8b716fe4ff185e5a
h@soloman <coding202020@gmail>
2020-02-17 22:11:53 +0200

    update the code

---
+++ js/About-me.js
@@ -0,0 +1,51 @@
+
+'use strict';
+var Q1 = prompt("Are you creative person?").toLowerCase();
+if (Q1 === 'yes'){
+    alert("Sounds good!");
+    console.log("Are you creative person? " + Q1);
+}else{
+    alert("You should be creative man, or you will in the future after loss alot of years");
+    console.log("Are you creative person? " + Q1);
+}
+
+var Q2 = prompt("Do you need advice?").toLowerCase();
+if (Q2 === 'yes'){
+    alert("This means that you are a superior person");
+    console.log("Do you need advice? "+ Q2);
+}else{
+    alert("perfection is for God only :)");
+    console.log("Do you need advice? "+ Q2);
+}
+
+var Q3 = prompt("Do you think advice that related to soft skills will help you ?").toLowerCase();
+if (Q3 === 'yes'){
+    alert("Godd choise, wee all need this kind of advice");
+    console.log("Do you think advice that related to soft skills will help you ? " + Q3);
+}else{
+alert("plz send to me in the email what kind of advice that you need");
+console.log("Do you think advice that related to soft skills will help you ? " + Q3);
+
+}
+
+var Q4 = prompt("I put for you Golden Box to write what you need to be a successful and positive person. Do you promise to use it ? :)").toLowerCase();
+if (Q4 === 'yes'){
+    alert("History will remind you");
+    console.log("I put for you Golden Box to write what you need to be a successful and positive person. Do you promise to use it ? " + Q4);
+
+}else{
+    alert("اتاكد انك قدمت لديوان الخدمة المدنية");
+    console.log("I put for you Golden Box to write what you need to be a successful and positive person. Do you promise to use it ? " + Q4);
+}
+
+var Q5 = prompt("If you reach this stage with yes answer, you schould proud of yourself, Do you agree ? :)").toLowerCase();
+if (Q5 === 'yes'){
+    alert("History will remind you");
+    console.log("If you reach this stage with yes answer, you schould proud of yourself, Do you agree ? " + Q5);
+
+}else{
+    alert("كل شغلي عالفاضي");
+    console.log("If you reach this stage with yes answer, you schould proud of yourself, Do you agree ? " + Q5);
+
+}
+
